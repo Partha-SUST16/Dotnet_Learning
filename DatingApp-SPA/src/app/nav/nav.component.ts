01e05609dc11bcaa09198a612d50599f044af84c
Partha-SUST16 <partha05@student.sust.edu>
2020-06-14 15:45:43 +0600

    using routes in component

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_service/auth.service';
 import { AlertifyService } from '../_service/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,8 @@ export class NavComponent implements OnInit {
   model: any = {};
   constructor(
     public authService: AuthService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {}
@@ -25,6 +27,9 @@ export class NavComponent implements OnInit {
       (error) => {
         this.alertify.error(error);
         console.log('failed to login', error);
+      },
+      () => {
+        this.router.navigate(['/members']);
       }
     );
   }
@@ -34,6 +39,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
-    console.log('logged out');
+    this.router.navigate(['/home']);
   }
 }
