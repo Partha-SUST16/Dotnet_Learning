591e720c1c348187d781fa91e5a940828a44019a
Partha-SUST16 <partha05@student.sust.edu>
2020-06-18 18:20:11 +0600

    timeago service filter added

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -47,6 +47,7 @@ namespace DatingApp.API
             });
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
+           
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
@@ -59,6 +60,7 @@ namespace DatingApp.API
                         ValidateAudience = false
                     };
                 });
+            services.AddScoped<LogUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
